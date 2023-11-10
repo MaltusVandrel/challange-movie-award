78440e2f4290367fcf37aa5b69b4a12021e3aa0c
Maltus Vandrel <matgoben@gmail.com>
2023-11-10 09:27:41 -0300

    Projeto iniciado

---
+++ src/main/java/com/texoit/challenge/ChallengeMovieAwardApplication.java
@@ -0,0 +1,14 @@
+package com.texoit.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeMovieAwardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeMovieAwardApplication.class, args);
+	}
+	
+	
+}
---
+++ src/test/java/com/texoit/challenge/ChallengeMovieAwardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.texoit.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeMovieAwardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
