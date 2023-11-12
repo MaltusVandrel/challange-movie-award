9512d96cb6ae4d529e260f0c0c10284612f90108
Matheus G Bento <matgoben@gmail.com>
2023-11-12 16:44:30 -0300

    Finalizado testes

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# challange-movie-award
-Challange TextoIT
+== Principal Tecnologias Utilizadas
+* Spring Boot
+* Maven
+
+== END-POINTS
+
+Obter produtores com maior e menor intervalo  entre dois prêmios consecutivos (HTTP GET)
+---
+http://localhost:8080/producer/awarded-producers
+---
+
+
+== Rodando o Projeto
+----
+
+1 - Importar projeto no eclipse o Eclipse
+2 - Baixar bibliotecas maven
+3 - Rodar como aplicativo springboot
+
+----
+
+== Testando o projeto
+----
+
+1 - Importar projeto no eclipse o Eclipse
+2 - Rodar teste JUnit
+
+----
+
+== Alterar dados do de entrada
+Localizado dentro da pasta src/main/resources substituir o arquivo "movielist.csv" por outro de mesmo nome com os dados desejados  
--- src/main/java/com/texoit/challenge/util/CSVUtil.java
+++ src/main/java/com/texoit/challenge/util/CSVUtil.java
@@ -6,6 +6,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class CSVUtil {
 	public static final int COLUMN_YEAR=0,COLUMN_TITLE=1,COLUMN_STUDIOS=2,COLUMN_PRODUCERS=3,COLUMN_WINNER=4;
 	public static final int COLUMN_NUMBER_IN_A_FILLED_ROW=5;
@@ -18,6 +20,7 @@ public class CSVUtil {
 	public static List<List<String>> toRows(InputStream file){
 		boolean isFirst=true;
 		List<List<String>> rows = new ArrayList<>();
+	
 		try (Scanner rowScanner = new Scanner(file)) {
 		    while (rowScanner.hasNextLine()) {
 		    	List<String> columns = new ArrayList<String>();
@@ -27,15 +30,63 @@ public class CSVUtil {
 		    	if(isFirst) {
 		    		isFirst=false;
 		    		continue;
-		    	}		    	
-		    	
+		    	}	
 		    	if(line==null)break;
 		    	columns.addAll(Arrays.asList(line.split(COLUMN_DELIMITER)));
 			    rows.add(columns);
 		    }
 		}
+		rows=getValidRows(rows);
 		return rows;
 	}
-	
+	private static List<List<String>> getValidRows(List<List<String>> rows){
+		List<List<String>> validRows=new ArrayList<List<String>>();
+		for (List<String> columns : rows) {
+			try {
+				String year = columns.get(CSVUtil.COLUMN_YEAR);
+				if(StringUtils.isBlank(year)||!StringUtils.isNumeric(year)){
+					continue;
+				}				
+			}catch(Exception e) {
+				continue;
+			}
+			try {
+				String title = columns.get(CSVUtil.COLUMN_TITLE);
+				if(StringUtils.isBlank(title)){
+					continue;
+				}				
+			}catch(Exception e) {
+				continue;
+			}
+			try {
+				String studios = columns.get(CSVUtil.COLUMN_STUDIOS);
+				if(StringUtils.isBlank(studios)){
+					continue;
+				}				
+			}catch(Exception e) {
+				continue;
+			}
+			try {
+				String producers = columns.get(CSVUtil.COLUMN_PRODUCERS);
+				if(StringUtils.isBlank(producers)){
+					continue;
+				}				
+			}catch(Exception e) {
+				continue;
+			}
+			if(columns.size()==CSVUtil.COLUMN_NUMBER_IN_A_FILLED_ROW) {
+				try {
+					String winner = columns.get(CSVUtil.COLUMN_WINNER);
+					if(StringUtils.isBlank(winner)||!CSVUtil.WORD_FOR_TRUE.equals(winner)){
+						continue;
+					}				
+				}catch(Exception e) {
+					continue;
+				}
+			}
+			validRows.add(columns);			
+		}
+		return validRows;
+	}
 	
 }
